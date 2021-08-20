5a089932301f28dbd9dfb877a35f661575748b94
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2021-08-20 16:39:20 +0200

    Commit Couleur Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ module.exports = {
     extend: {
       fontFamily: {
       headline: ['Rampart One']
-}
-
-    }
+    },
+    colors : {
+      mainColor : '#212f49'
+    },
   },
   variants: {
     extend: {},
